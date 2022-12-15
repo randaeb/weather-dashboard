eddf21676be2bc52a0094401fb1bc60b56940646
Randa Bukowski <randabukowski@Randas-MacBook-Pro.local>
2022-12-14 23:04:43 -0800

    boilerplate

---
+++ script.js
@@ -0,0 +1,21 @@
+var APIKey = "0cccd73fe11331f8d818d116fcd9caa5";
+
+var city = 
+
+//local storage
+
+//lat and long coordinates 
+
+//fetch weather 
+
+//search history 
+
+// weather icons 
+
+//DOM
+
+
+
+
+fetch(URL)
+.then(response)
